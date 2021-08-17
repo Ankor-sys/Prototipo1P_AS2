e2e215781197dedffb5c0265e9315f925a789234
Ankor-sys <sebasmoreira4@gmail.com>
2021-08-17 09:59:52 -0600

    Creación de MDI y agregando script de BD a carpeta de proyecto

---
+++ Prototipo1P/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo1P
+{
+    
+    public partial class Form1 : Form
+    {
+        private frmVentas forma;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void realizarVentaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(forma == null)
+            {
+                forma = new frmVentas();
+                forma.MdiParent = this;
+                forma.FormClosed += new FormClosedEventHandler(CerrarForma);
+                forma.Show();
+            }
+            else
+            {
+                forma.Activate();
+            }
+        }
+
+        void CerrarForma(object sender, FormClosedEventArgs e)
+        {
+            forma = null;
+        }
+    }
+}
