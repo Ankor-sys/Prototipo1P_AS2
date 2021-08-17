f78d74708c26003f5cb11b09b722f6c8f9f3cdd0
Ankor-sys <sebasmoreira4@gmail.com>
2021-08-17 11:07:01 -0600

    Creación de form de Ventas

---
+++ Prototipo1P/frmVentas.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace Prototipo1P
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.txtCodigoCliente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgProductos = new System.Windows.Forms.DataGridView();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCodigoProducto = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCodigoBodega = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Documento:";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(92, 44);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(142, 20);
+            this.txtDocumento.TabIndex = 1;
+            // 
+            // txtCodigoCliente
+            // 
+            this.txtCodigoCliente.Location = new System.Drawing.Point(92, 70);
+            this.txtCodigoCliente.Name = "txtCodigoCliente";
+            this.txtCodigoCliente.Size = new System.Drawing.Size(142, 20);
+            this.txtCodigoCliente.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Codigo Cliente:";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(92, 96);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(142, 20);
+            this.txtFecha.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Codigo Cliente:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(300, 26);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ENCABEZADO DE VENTA";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(92, 148);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(142, 20);
+            this.txtStatus.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Status:";
+            // 
+            // dgProductos
+            // 
+            this.dgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProductos.Location = new System.Drawing.Point(363, 21);
+            this.dgProductos.Name = "dgProductos";
+            this.dgProductos.Size = new System.Drawing.Size(415, 147);
+            this.dgProductos.TabIndex = 11;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(405, 171);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(142, 20);
+            this.txtTotal.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(365, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Total:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 217);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(241, 26);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "DETALLE DE VENTA";
+            // 
+            // txtCodigoProducto
+            // 
+            this.txtCodigoProducto.Location = new System.Drawing.Point(92, 260);
+            this.txtCodigoProducto.Name = "txtCodigoProducto";
+            this.txtCodigoProducto.Size = new System.Drawing.Size(142, 20);
+            this.txtCodigoProducto.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 263);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Codigo Producto:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(92, 286);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(142, 20);
+            this.txtCantidad.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 289);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Cantidad:";
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(92, 312);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(142, 20);
+            this.txtCosto.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(3, 315);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(37, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Costo:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(92, 338);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(142, 20);
+            this.txtPrecio.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(3, 341);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 13);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Precio:";
+            // 
+            // txtCodigoBodega
+            // 
+            this.txtCodigoBodega.Location = new System.Drawing.Point(92, 364);
+            this.txtCodigoBodega.Name = "txtCodigoBodega";
+            this.txtCodigoBodega.Size = new System.Drawing.Size(142, 20);
+            this.txtCodigoBodega.TabIndex = 24;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(3, 367);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(83, 13);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "Codigo Bodega:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(122, 400);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 25;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(636, 174);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(142, 36);
+            this.btnFinalizar.TabIndex = 26;
+            this.btnFinalizar.Text = "Finalizar Venta";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(331, 312);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(465, 30);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "ESTADO DE PROCESO: Incompleto";
+            // 
+            // frmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 450);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCodigoBodega);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtCodigoProducto);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dgProductos);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCodigoCliente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.label1);
+            this.Name = "frmVentas";
+            this.Text = "frmVentas";
+            this.Load += new System.EventHandler(this.frmVentas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.TextBox txtCodigoCliente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgProductos;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtCodigoProducto;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCodigoBodega;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label label14;
+    }
+}
---
+++ Prototipo1P/frmVentas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo1P
+{
+    public partial class frmVentas : Form
+    {
+        Conexion cn = new Conexion();
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVentas_Load(object sender, EventArgs e)
+        {
+            dgProductos.Columns.Add("codigo_producto", "Codigo");
+            dgProductos.Columns.Add("cantidad_producto", "Cantidad");
+            dgProductos.Columns.Add("costo_ventadet", "Costo");
+            dgProductos.Columns.Add("precio_ventadet", "Precio");
+            dgProductos.Columns.Add("codigo_bodega", "Bodega");
+
+        }
+
+        public void llenarGrid()
+        {
+            string cadena = "";/*INSERT INTO" +
+                " empleado (id_empleado, cui_empleado, nit_empleado, nombre_empleado, apellido_empleado," +
+                " genero_empleado, edad_empleado, telefono_empleado, direccion_empleado, email_empleado," +
+                " status_empleado, id_puesto, colegiado_empleado, id_sede) VALUES (" +
+                "'" + txtIdEmpleado.Text + "', '"
+                 + txtCui.Text + "', '"
+                 + txtNit.Text + "', '"
+                 + txtNombre.Text + "', '"
+                 + txtApellido.Text + "', '"
+                 + txtGenero.Text + "', "
+                 + int.Parse(txtEdad.Text) + " , '"
+                 + txtTelefono.Text + "', '"
+                 + txtDireccion.Text + "', '"
+                 + txtEmail.Text + "', '"
+                 + txtStatus.Text + "', '"
+                 + lblIdPuesto.Content + "', '"
+                 + txtColegiado.Text + "', '"
+                 + lblIdSede.Content + "' ); ";*/
+
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+            MessageBox.Show("Inserción realizada");
+        }
+    }
+}
